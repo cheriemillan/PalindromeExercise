ab72d65a903c09c74b8ddeceec174b02f59cb464
CherieMillan <cherie99millan@gmail.com>
2024-08-01 14:48:33 -0400

    Made a unit test to show if a string is the same reversed

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace PalindromeExercise;
+
+public class WordSmith
+{
+    public bool IsAPalindrome(string answer)
+    {
+        string reversed = new string(answer.Reverse().ToArray());
+        return answer.Equals(reversed, StringComparison.OrdinalIgnoreCase);
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,23 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("brick", false)]
+        [InlineData("madam", true)]
+        public void Test1(string answer, bool expected)
         {
+            WordSmith wordSmith = new WordSmith();
+
+            bool actual = wordSmith.IsAPalindrome(answer);
+            
+            Assert.Equal(expected, actual);
+
 
         }
     }
